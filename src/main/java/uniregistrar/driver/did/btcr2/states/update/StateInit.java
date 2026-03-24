b9617586e47230f6ec127255d753779848d3a940
Markus Sabadello <markus@danubetech.com>
2026-03-25 00:14:28 +0100

    Continue work on support for "update" operation

--- src/main/java/uniregistrar/driver/did/btcr2/states/update/StateInit.java
+++ src/main/java/uniregistrar/driver/did/btcr2/states/update/StateInit.java
@@ -1,47 +1,42 @@
 package uniregistrar.driver.did.btcr2.states.update;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import foundation.identity.did.DID;
-import foundation.identity.did.DIDDocument;
+import foundation.identity.did.DIDDocumentV1_1;
 import foundation.identity.did.parser.ParserException;
-import fr.acinq.secp256k1.Hex;
-import io.ipfs.multibase.Multibase;
+import jakarta.json.Json;
+import jakarta.json.JsonPatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uniregistrar.RegistrationException;
-import uniregistrar.driver.did.btcr2.Network;
 import uniregistrar.driver.did.btcr2.connections.bitcoin.BitcoinConnection;
 import uniregistrar.driver.did.btcr2.connections.bitcoin.BitcoinConnector;
 import uniregistrar.driver.did.btcr2.crud.update.Update;
 import uniregistrar.driver.did.btcr2.data.records.IdentifierComponents;
 import uniregistrar.driver.did.btcr2.job.Job;
 import uniregistrar.driver.did.btcr2.job.JobRegistry;
-import uniregistrar.driver.did.btcr2.ledger.DidDocUnAssembler;
 import uniregistrar.driver.did.btcr2.syntax.DidBtcr2IdentifierDecoding;
-import uniregistrar.driver.did.btcr2.util.MulticodecUtil;
-import uniregistrar.openapi.model.DidDocument;
+import uniregistrar.openapi.model.SigningResponse;
 import uniregistrar.openapi.model.UpdateRequest;
 import uniregistrar.openapi.model.UpdateState;
+import uniregistrar.openapi.model.VerificationMethodPublicData;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class StateInit {
 
     private static final Logger log = LoggerFactory.getLogger(StateInit.class);
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final JsonMapper jsonMapper = JsonMapper.builder()
+            .defaultPropertyInclusion(JsonInclude.Value.ALL_NON_NULL)
+            .build();
 
     public static final int STATE = 0;
 
-    static {
-        objectMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
-    }
-
     public static UpdateState update(JobRegistry jobRegistry, Job job, UpdateRequest updateRequest, Update update, BitcoinConnector bitcoinConnector) throws RegistrationException {
 
-        // read input fields
+        // check input DID
 
         DID did;
         try {
@@ -49,30 +44,41 @@ public class StateInit {
         } catch (ParserException ex) {
             throw new RegistrationException(RegistrationException.ERROR_INVALID_DID, "Invalid DID: " + updateRequest.getDid());
         }
-        List<DidDocument> didDocumentList = updateRequest.getDidDocument();
-        DIDDocument didDocument = (didDocumentList == null || didDocumentList.isEmpty()) ? null : objectMapper.convertValue(didDocumentList.getFirst(), DIDDocument.class);
-
-        // parse DID
 
         IdentifierComponents identifierComponents = DidBtcr2IdentifierDecoding.didBtcr2IdentifierDecoding(did);
+
+        // check input DID document update operations
+
+        List<JsonPatch> jsonPatches = new ArrayList<>();
+        if (updateRequest.getDidDocumentOperation() != null && updateRequest.getDidDocument() != null) {
+            for (int i=0; i<updateRequest.getDidDocumentOperation().size(); i++) {
+                String didDocumentOperation = updateRequest.getDidDocumentOperation().get(i);
+                DIDDocumentV1_1 didDocument = jsonMapper.convertValue(updateRequest.getDidDocument().get(i), DIDDocumentV1_1.class);
+                if (! "patchDidDocument".equals(didDocumentOperation)) throw new RegistrationException(RegistrationException.ERROR_INVALID_OPTIONS, "Unsupported DID document operation: " + didDocumentOperation);
+                JsonPatch jsonPatch = Json.createPatch(Json.createArrayBuilder(Collections.singletonList(didDocument.getJsonObject())).build());
+                jsonPatches.add(jsonPatch);
+            }
+        }
+
+        // check options and secret
+
+        Integer targetVersionId = updateRequest.getOptions() == null ? null : (Integer) updateRequest.getOptions().getAdditionalProperties().get("targetVersionId");
+
+        VerificationMethodPublicData verificationMethodPublicData = null;
+        SigningResponse signingResponse = null;
+
+        if (updateRequest.getSecret() != null && updateRequest.getSecret().getVerificationMethod() != null && ! updateRequest.getSecret().getVerificationMethod().isEmpty()) verificationMethodPublicData = updateRequest.getSecret().getVerificationMethod().getFirst().getVerificationMethodPublicData();
+        if (updateRequest.getSecret() != null && updateRequest.getSecret().getSigningResponse() != null && ! updateRequest.getSecret().getSigningResponse().isEmpty()) signingResponse = updateRequest.getSecret().getSigningResponse().get("didUpdate");
 
         // find Bitcoin connection
 
         BitcoinConnection bitcoinConnection = bitcoinConnector.getBitcoinConnection(identifierComponents.network());
         if (bitcoinConnection == null) throw new RegistrationException(RegistrationException.ERROR_INVALID_DID, "Unknown network: " + identifierComponents.network());
 
-        // unassemble initialKey
+        // prepare didSourceDocument
 
-        byte[] unassembledInitialKey = DidDocUnAssembler.unassembleInitialKey(didDocument);
-        if (unassembledInitialKey == null) {
-            // next state
-            return TransitionInit.transitionToInitGetVerificationMethod(bitcoinConnection);
-        }
-
-        // prepare sourceDocument
-
-        DIDDocument sourceDocument = didDocument;
-        if (log.isDebugEnabled()) log.debug("sourceDocument: {}", sourceDocument == null ? null : sourceDocument.toJson());
+        DIDDocumentV1_1 didSourceDocument = didDocument;
+        if (log.isDebugEnabled()) log.debug("didSourceDocument: {}", didSourceDocument == null ? null : didSourceDocument.toJson());
 
         // DID DOCUMENT METADATA
 
@@ -80,7 +86,15 @@ public class StateInit {
 
         // update
 
-        List<Map<String, Object>> signalsMetadata = update.update(did, sourceDocument, /* TODO */ null, /* TODO */ null, /* TODO */ null, /* TODO */ null, didDocumentMetadata);
+        try {
+            update.update(didSourceDocument, jsonPatches, targetVersionId, verificationMethodPublicData, signingResponse, didDocumentMetadata);
+        } catch (Update.GetVerificationMethodException ex) {
+            // TODO
+            throw new RuntimeException(ex);
+        } catch (Update.SignPayloadException ex) {
+            // TODO
+            throw new RuntimeException(ex);
+        }
 
         // next state
 
---
+++ src/main/java/uniregistrar/driver/did/btcr2/util/JSONPatchUtil.java
@@ -0,0 +1,19 @@
+package uniregistrar.driver.did.btcr2.util;
+
+import foundation.identity.did.DIDDocumentV1_1;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonPatch;
+
+import java.io.StringWriter;
+
+public class JSONPatchUtil {
+
+    public static DIDDocumentV1_1 apply(DIDDocumentV1_1 didDocument, JsonPatch jsonPatch) {
+        JsonObject didDocumentObject = Json.createObjectBuilder(didDocument.toMap()).build();
+        JsonObject patchedDidDocumentObject = jsonPatch.apply(didDocumentObject);
+        StringWriter stringWriter = new StringWriter();
+        Json.createWriter(stringWriter).write(patchedDidDocumentObject);
+        return DIDDocumentV1_1.fromJson(stringWriter.toString());
+    }
+}
