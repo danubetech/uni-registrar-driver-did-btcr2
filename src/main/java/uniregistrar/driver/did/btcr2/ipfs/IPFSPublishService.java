35b5e39bda3349ccf4e813636726c02e13728bdd
Markus Sabadello <markus@danubetech.com>
2026-04-20 18:02:47 +0200

    IPFS publishing function

--- src/main/java/uniregistrar/driver/did/btcr2/ipfs/IPFSPublishService.java
+++ src/main/java/uniregistrar/driver/did/btcr2/ipfs/IPFSPublishService.java
@@ -1,4 +1,23 @@
 package uniregistrar.driver.did.btcr2.ipfs;
 
+import io.ipfs.api.NamedStreamable;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class IPFSPublishService {
+
+    private static final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    public static void publish(IPFSConnection ipfsConnection, byte[] payload) throws IOException {
+
+        executorService.submit(() -> {
+            try {
+                ipfsConnection.getIpfs().add(new NamedStreamable.ByteArrayWrapper(payload));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex.getMessage(), ex);
+            }
+        });
+    }
 }
