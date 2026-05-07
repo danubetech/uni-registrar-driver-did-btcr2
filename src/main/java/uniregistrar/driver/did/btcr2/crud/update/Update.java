ff97caf686cf22ec204f6f15539a3633d6154118
Markus Sabadello <markus@danubetech.com>
2026-05-07 13:07:07 +0200

    Further fix beacon service selection

--- src/main/java/uniregistrar/driver/did/btcr2/crud/update/Update.java
+++ src/main/java/uniregistrar/driver/did/btcr2/crud/update/Update.java
@@ -286,7 +286,7 @@ public class Update {
 
         Stream<Service> serviceStream = didSourceDocument.getServices().stream();
         if (beaconServiceId != null) {
-            serviceStream = serviceStream.filter(service -> JsonLDDereferencer.findByIdInJsonLdObject(didSourceDocument, beaconServiceId, didSourceDocument.getId()) != null);
+            serviceStream = serviceStream.filter(service -> service.equals(JsonLDDereferencer.findByIdInJsonLdObject(didSourceDocument, beaconServiceId, didSourceDocument.getId())));
         }
         if (beaconServiceType != null) {
             serviceStream = serviceStream.filter(service -> beaconServiceType.equals(service.getType()));
