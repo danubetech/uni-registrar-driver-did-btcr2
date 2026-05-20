ac7576ded5a93bef4172dcb76042ae6f5fa6e92f
Markus Sabadello <markus@danubetech.com>
2026-05-20 23:53:24 +0200

    Update log encoding

--- src/main/java/uniregistrar/driver/did/btcr2/aggregation/AggregationCohort.java
+++ src/main/java/uniregistrar/driver/did/btcr2/aggregation/AggregationCohort.java
@@ -400,9 +400,9 @@ public class AggregationCohort {
             BytesArray updateHash = this.getSmtUpdateHashes().get(i);
             if (log.isDebugEnabled()) log.debug("Inserting didIndex {} with updateHash {}", Base64.getUrlEncoder().withoutPadding().encodeToString(didIndex.bytes()), Base64.getUrlEncoder().withoutPadding().encodeToString(updateHash.bytes()));
             sparseMerkleTree.insertUpdate(didIndex.bytes(), updateHash.bytes());
-            if (log.isDebugEnabled()) log.debug("Generating proof for didIndex {} with updateHash {}", Base64.getUrlEncoder().withoutPadding().encodeToString(didIndex.bytes()), Base64.getUrlEncoder().withoutPadding().encodeToString(updateHash.bytes()));
+            if (log.isDebugEnabled()) log.debug("Generating proof for didIndex {} with updateHash {}", Hex.encodeHexString(didIndex.bytes()), Base64.getUrlEncoder().withoutPadding().encodeToString(updateHash.bytes()));
             SmtProof smtProof = sparseMerkleTree.generateProofForIndex(didIndex.bytes());
-            if (log.isDebugEnabled()) log.debug("Generated proof for didIndex {} with updateHash {}: {}", Base64.getUrlEncoder().withoutPadding().encodeToString(didIndex.bytes()), Base64.getUrlEncoder().withoutPadding().encodeToString(updateHash.bytes()), smtProof.toString());
+            if (log.isDebugEnabled()) log.debug("Generated proof for didIndex {} with updateHash {}: {}", Hex.encodeHexString(didIndex.bytes()), Base64.getUrlEncoder().withoutPadding().encodeToString(updateHash.bytes()), smtProof.toString());
             this.smtProofs.put(didIndex, smtProof);
         }
 
